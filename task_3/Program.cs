cdf659d8fb71eba9416b4788b0900b84f86c5912
kalinkineo <kalinkineo@gmail.com>
2022-07-14 22:10:28 +0300

    add task1, task3, task5, task7

---
+++ task_3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 3. Напишите программу, которая будет выдавать
+// название дня недели по заданному номеру.
+// 3 -> Среда
+// 5 -> Пятница
+
+
+Console.Write("Введите порядковый номер дня недели: ");
+int day = Convert.ToInt32(Console.ReadLine());
+
+if (day == 1)
+    Console.WriteLine("понедельник");
+else if(day == 2)
+    Console.WriteLine("вторник");
+else if(day == 3)
+    Console.WriteLine("среда");
+else if(day == 4)
+    Console.WriteLine("четверг");
+else if(day == 5)
+    Console.WriteLine("пятница");
+else if(day == 6)
+    Console.WriteLine("суббота");
+else if(day == 7)
+    Console.WriteLine("воскресенье");  
+else Console.WriteLine("нет такого дня недели"); 
---
+++ task_5/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 5. Напишите программу, которая на вход принимает
+// одно число (N), а на выходе показывает все целые
+// числа в промежутке от -N до N.
+// 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
+// 2 -> " -2, -1, 0, 1, 2"
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int temp = 0;
+
+if (num < 0) {
+    temp = num;
+    num = -num;
+}
+else 
+{
+    temp = -num;
+}
+
+while (temp <= num){
+    Console.Write($"{temp} ");
+    temp++;
+}
+
+
+
