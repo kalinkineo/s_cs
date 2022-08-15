0876eb6bb1703717b134abfbd105e2d47964c1ad
kalinkineo <kalinkineo@gmail.com>
2022-08-15 20:55:37 +0300

    add task_44, 45 project

---
+++ task_44/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 44: Не используя рекурсию, выведите первые N чисел
+// Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+// Если N = 5 -> 0 1 1 2 3
+// Если N = 3 -> 0 1 1
+// Если N = 7 -> 0 1 1 2 3 5 8
+
+Console.Clear();
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+
+void Fibon(int numNew)
+{
+    int num1 = 0;
+    int num2 = 1;
+    Console.Write($"{num1} {num2} ");
+    for (int i = 0; i < numNew - 2; i++)
+    {
+        int nextEl = num1 + num2;
+        Console.Write($"{nextEl} ");
+        num1 = num2;
+        num2 = nextEl;
+    }
+}
+
+Fibon(num);
---
+++ task_45/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 45: Напишите программу, которая будет создавать
+// копию заданного массива с помощью поэлементного
+// копирования.
+
+int[] CopyArray (int[] array)
+{
+    int[] copyArr = new int[array.Length];
+
+    for (int i = 0; i < copyArr.Length; i++)
+    {
+        copyArr[i] = array[i];
+    }
+    return copyArr;
+}
