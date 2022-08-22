7b0124fe70575c651c1cd785c21c44ec0cf521a2
kalinkineo <kalinkineo@gmail.com>
2022-08-22 22:04:31 +0300

    add task_30 project

---
+++ task_30/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 30: Напишите программу, которая
+// выводит массив из 8 элементов, заполненный
+// нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+int[] array = new int[20];
+
+for (int i = 0; i < array.Length; i++)
+{
+    int num = new Random().Next(0, 2);
+    array[i] = num;
+}
+
+Console.Write("[");
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write(array[i]);
+    if (i != array.Length - 1)
+        Console.Write(",");
+}
+
+Console.Write("]");
