d9e82a08cb5726b096a4ceafc98fb7e62bbe758e
kalinkineo <kalinkineo@gmail.com>
2022-08-22 21:15:25 +0300

    add task_26 project

---
+++ task_26/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 26: Напишите программу, которая принимает
+// на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"{num} -> {GetDigitCount(num)}");
+
+int GetDigitCount(int num)
+{
+    int result = 0;
+
+    while(num >= 1)
+    {
+        num = num / 10;
+        result++;
+    }
+
+    return result;
+}
