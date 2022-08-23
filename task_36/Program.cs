bba095fa2f04bee43457a13459b7b45efbd01639
kalinkineo <kalinkineo@gmail.com>
2022-08-23 22:25:52 +0300

    add task_36 project

---
+++ task_36/Program.cs
@@ -0,0 +1,60 @@
+﻿// Задача 36: Задайте одномерный массив,
+// заполненный случайными числами.
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] GetArray(int count)
+{
+    int[] array = new int[count];
+    
+    for (int i = 0; i < array.Length; i++)
+    {
+        count = new Random().Next(-100, 100);
+        array[i] = count;
+    }
+
+    return array;
+}
+
+int GetSumOddPos(int[] array_num)
+{
+    int sum = 0;
+int[] array_num2 = [-4, -6, 89, 6];
+    for (int i = 0; i < array_num.Length; i++)
+    {
+        sum = sum + array_num[i];
+        i++;
+    }
+    return sum;
+}
+
+void OutputResult(int[] outpuntArray, int countEven)
+{
+    Console.Write("{");
+    for (int i = 0; i < outpuntArray.Length; i++)
+    {
+        Console.Write(outpuntArray[i]);
+        if (i != outpuntArray.Length - 1)
+        {
+            Console.Write(",");
+        }
+        else
+        {
+            Console.Write("}");
+        }
+    }
+
+    Console.WriteLine($" -> {countEven}");
+}
+
+Console.Clear();
+
+Console.WriteLine("Введите количество элементов массива:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int[] randomArray = GetArray(num);
+int sumOddPos = GetSumOddPos(randomArray);
+
+OutputResult(randomArray, sumOddPos);
