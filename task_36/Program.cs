621bb809afc3a1e17aecfc404b6c89250fc3f0fc
kalinkineo <kalinkineo@gmail.com>
2022-08-25 23:35:23 +0300

    fix task_36

--- task_36/Program.cs
+++ task_36/Program.cs
@@ -1,28 +1,26 @@
 ﻿// Задача 36: Задайте одномерный массив,
 // заполненный случайными числами.
 // Найдите сумму элементов, стоящих на нечётных позициях.
-
 // [3, 7, 23, 12] -> 19
 // [-4, -6, 89, 6] -> 0
 
-int[] GetArray(int count)
+int[] CreateArrayRndInd(int size, int min, int max)
 {
-    int[] array = new int[count];
-    
-    for (int i = 0; i < array.Length; i++)
-    {
-        count = new Random().Next(-100, 100);
-        array[i] = count;
-    }
+    int[] arr = new int[size];
+    Random rnd = new Random();
 
-    return array;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
 }
 
 int GetSumOddPos(int[] array_num)
 {
     int sum = 0;
 
-    for (int i = 0; i < array_num.Length; i++)
+    for (int i = 1; i < array_num.Length; i++)
     {
         sum = sum + array_num[i];
         i++;
@@ -30,23 +28,14 @@ int GetSumOddPos(int[] array_num)
     return sum;
 }
 
-void OutputResult(int[] outpuntArray, int countEven)
+void PrintArray(int[] array)
 {
-    Console.Write("{");
-    for (int i = 0; i < outpuntArray.Length; i++)
+    for (int i = 0; i < array.Length; i++)
     {
-        Console.Write(outpuntArray[i]);
-        if (i != outpuntArray.Length - 1)
-        {
-            Console.Write(",");
-        }
-        else
-        {
-            Console.Write("}");
-        }
+        if (i == 0) Console.Write("[");
+        if (i < array.Length - 1) Console.Write(array[i] + ", ");
+        else Console.Write(array[i] + "]");
     }
-
-    Console.WriteLine($" -> {countEven}");
 }
 
 Console.Clear();
@@ -54,7 +43,8 @@ Console.Clear();
 Console.WriteLine("Введите количество элементов массива:");
 int num = Convert.ToInt32(Console.ReadLine());
 
-int[] randomArray = GetArray(num);
-int sumOddPos = GetSumOddPos(randomArray);
+int[] arrayRndInd = CreateArrayRndInd(num, -20, 20);
+int sumOddPos = GetSumOddPos(arrayRndInd);
 
-OutputResult(randomArray, sumOddPos);
+PrintArray(arrayRndInd);
+Console.WriteLine($" -> {sumOddPos}");
