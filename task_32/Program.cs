143c9a17a8079fa6a92beabee766fc620c05bd4c
kalinkineo <kalinkineo@gmail.com>
2022-08-25 22:00:21 +0300

    add task_32

---
+++ task_32/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 32: Напишите программу замена элементов
+// массива: положительные элементы замените на
+// соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+int[] CreateArrayRndInd(int size, int min, int max) // 1 пункт
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void GetRivers(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = array[i] * -1;
+    }
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i == 0) Console.Write("[");
+        if (i < array.Length - 1) Console.Write(array[i] + ", ");
+        else Console.WriteLine(array[i] + "]");
+    }
+}
+
+int[] array = CreateArrayRndInd(4, -100, 100);
+
+Console.Clear();
+PrintArray(array);
+GetRivers(array);
+Console.WriteLine();
+PrintArray(array);
