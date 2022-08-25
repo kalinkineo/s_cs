27ac6e523e77ec7072f0c2ca88bb1fa7910f8e97
kalinkineo <kalinkineo@gmail.com>
2022-08-25 22:05:08 +0300

    add task_33

--- task_33/Program.cs
+++ task_33/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 33: Задайте массив. Напишите программу, которая
+// определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// 3; массив [6, 7, 19, 345, 3] -> да
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int[] CreateArrayRndInd(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void Elem(int[] array, int search)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] == search)
+        {
+            Console.WriteLine($"Числа {search} есть в массиве");
+            return;
+        }
+    }
+    Console.WriteLine($"Числа {search} нет в массиве");
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i == 0) Console.Write("[");
+        if (i < array.Length - 1) Console.Write(array[i] + ", ");
+        else Console.Write(array[i] + "]");
+    }
+}
+
+int[] array = CreateArrayRndInd(12, 0, 6);
+PrintArray(array);
+Console.WriteLine();
+Elem(array, num);
